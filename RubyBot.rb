358d0a7fca939b060bc2ecff0b81fcb4d1061504
clindsay107 <clindsay107@gmail.com>
2013-09-20 13:39:13 -0700

    Refactored most methods into separate classes/modules

--- RubyBot.rb
+++ RubyBot.rb
@@ -1,38 +1,24 @@
+$LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib")
+require 'irc_commands'
+require 'triggers'
 require 'socket'
 
 class IRCBot
 
 	def initialize(name, server, port, channel)
-		@channel = channel
-		@name = name
-		@socket = TCPSocket.open(server, port)
+		@@channel = channel
+		@@name = name
+		@@socket = TCPSocket.open(server, port)
 		@inChan = false
-		say "NICK #{name}"
-		say "USER #{name} 0 * #{name}"
-		say "JOIN ##{channel}"
-	end
-
-	#say message to server
-	def say(message)
-		puts 'SERV >> ' + message
-		@socket.puts message
-	end
-
-	#say message to specified channel
-	def say_in_chan(message)
-		if @inChan
-			say "PRIVMSG ##{@channel} :#{message}"
-		end
-	end
-
-	def quit(quitmsg = "cya nerds")
-		say_in_chan quitmsg
-		say "Quit"
+		IRCcommands.set_class_vars(@@socket, channel, name)
+		IRCcommands.say "NICK #{name}"
+		IRCcommands.say "USER #{name} 0 * #{name}"
+		IRCcommands.say "JOIN ##{channel}"
 	end
 
 	def run
-		until @socket.eof? do
-			message = @socket.gets
+		until @@socket.eof? do
+			message = @@socket.gets
 			puts 'SERV << ' + message
 
 			#kind of a hackish way to determine but it works
@@ -41,16 +27,8 @@ class IRCBot
 				puts 'INFO >> Joined channel successfuly'
 			end
 
-			if message.include?('PING')
-				say 'PONG :pingis \n'
-			end
-
-			if message.include?('hi ' + @name)
-				say_in_chan 'shut up nerd'
-			end
-
-			if message.include?('now quit')
-				quit
+			if @inChan
+				Triggers.parse_message(message)
 			end
 
 		end
