4cf309a208337e648d538f176527c77a85e20a3d
clindsay107 <clindsay107@gmail.com>
2013-10-04 14:11:49 -0700

    Fixed parsing to accept multiple triggers for a proc. Also added the dumbest trigger ever because Blice said to

--- lib/triggers.rb
+++ lib/triggers.rb
@@ -2,16 +2,19 @@ require 'irc_commands'
 require 'database'
 require 'link_grabber'
 require 'decision'
+require 'define'
 
 class Triggers
 
 	@triggers = {
-		"hi #{@name}" => proc { hello },
-		"now quit" => proc { quit },
-		"!admins" => proc { Database.active_admins },
-		"http" => proc { LinkGrabber.read_HTML(@msg) },
-		"!decide" => proc { Decision.decide(@msg) },
-		"!8ball" => proc { Decision.eight_ball }
+		["hi #{@name}"] => proc { hello },
+		["now quit"] => proc { quit },
+		["!admins"] => proc { Database.active_admins },
+		["http"] => proc { LinkGrabber.read_HTML(@msg) },
+		["!decide"] => proc { Decision.decide(@msg) },
+		["!8ball"] => proc { Decision.eight_ball },
+		["weed", "pot", "ganja", "420", "marijuana"] => proc { four_twenty },
+		["!define"] => proc { Define.define(@msg) }
 		}
 
 	def self.set_name(name)
@@ -30,11 +33,9 @@ class Triggers
 			@chan = @parts[2]
 			@msg = @parts[3..-1].join(" ")[1..-1]
 
-			#puts "MSG << #{@msg}"
-
 			#search the message for triggers, and call proc
 			@triggers.each do |k, v|
-				if @msg.downcase.include?(k)
+				if k.any? {|t| @msg.downcase.include?(t) }
 					v.call
 				end
 			end
@@ -56,6 +57,17 @@ class Triggers
 		else
 			IRCcommands.say_in_chan("You aren't allowed to issue that command, sorry")
 		end
+	end
+
+	#the worst trigger ever made in the history of irc
+	def self.four_twenty
+		phrases = ["so skoned right now", "ya im hi (on pot )", "is this still cherried ?",
+			"wat kinda papers r these?", "ya its #kush lol", "wats up my blaze brother",
+			"box hotting my room rite now lmao soo smokey B)", "4 2 0  T I L L  I  D I E",
+			"ya i do pot/weed", "hit this bro its fucken frosty", "fuck im rly hi (on weed)",
+			"u do ganja rite ??", "jesus = dank", "weed is naturl bro its good 4 u",
+			"if ur not smokin ur jokin...", "fuk i think im too hi call 911"]
+		IRCcommands.say_in_chan(phrases.sample)
 	end
 
 end
