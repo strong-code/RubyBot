1585c29867b2e59240e81919975f3e0008f55629
clindsay107 <clindsay107@gmail.com>
2013-10-04 12:51:42 -0700

    Now ignores images because why would we need to GET images?

--- lib/link_grabber.rb
+++ lib/link_grabber.rb
@@ -5,15 +5,24 @@ class LinkGrabber
 
 	#open and read HTML from a given url
 	def self.read_HTML(url, limit = 10)
+		return if /\.(jpg|jpeg|bmp|png|tiff$)/.match(url) != nil
+
 		url = url.sub(/^https/, "http") if url.include?("https")
 
 		raise ArgumentError, "Redirected more than #{10} times, failed to fetch html" if limit == 0
 
-		response = Net::HTTP.get_response(URI(url))
+		begin
+			response = Net::HTTP.get_response(URI(url))
+		rescue URI::InvalidURIError
+			puts "ERR >> Invalide URI: #{url}"
+			IRCcommands.say_in_chan("That looks like an invalid URL m8")
+			return
+		end
 
 		case response
 		when Net::HTTPSuccess then
 			html = response.read_body
+			puts "hurrr im searching for title anyways"
 			title = /<title>(.*)<\/title>/.match(html)[1]
 			IRCcommands.say_in_chan("\x02Page Title:\x02 #{title}")
 		when Net::HTTPRedirection then
@@ -25,6 +34,5 @@ class LinkGrabber
 			puts "ERR >> Unable to reach #{url}. Received #{response.value}"
 		end
 	end
-
 
 end
