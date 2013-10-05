90f6fac97c405a9aa5752a248066c5dfdae7160b
clindsay107 <clindsay107@gmail.com>
2013-10-05 11:37:10 -0700

    Added .gif to ignored files, fixed bounce limit exception

--- lib/link_grabber.rb
+++ lib/link_grabber.rb
@@ -5,26 +5,31 @@ class LinkGrabber
 
 	#open and read HTML from a given url
 	def self.read_HTML(url, limit = 10)
-		return if /\.(jpg|jpeg|bmp|png|tiff$)/.match(url) != nil
+		return if /\.(jpg|jpeg|bmp|png|tiff|gif$)/.match(url) != nil
 
 		url = url.sub(/^https/, "http") if url.include?("https")
 
-		raise ArgumentError, "Redirected more than #{10} times, failed to fetch html" if limit == 0
+		if limit == 0
+			puts "ERR >> Redirected too many times, failed to fetch HTML"
+			return IRCcommands.say_in_chan("Redirected too many times, I won't put up with this shit")
+		end
 
 		begin
 			response = Net::HTTP.get_response(URI(url))
 		rescue URI::InvalidURIError
 			puts "ERR >> Invalide URI: #{url}"
-			IRCcommands.say_in_chan("That looks like an invalid URL m8")
 			return
 		end
 
 		case response
 		when Net::HTTPSuccess then
 			html = response.read_body
-			puts "hurrr im searching for title anyways"
-			title = /<title>(.*)<\/title>/.match(html)[1]
-			IRCcommands.say_in_chan("\x02Page Title:\x02 #{title}")
+			begin
+				title = /<title>(.*)<\/title>/.match(html)[1]
+				return IRCcommands.say_in_chan("\x02Page Title:\x02 #{title}")
+			rescue NoMethodError
+				return puts "Unable to fetch page title"
+			end
 		when Net::HTTPRedirection then
 			location = response['location']
 			puts "INFO >> Redirected to #{location}"
